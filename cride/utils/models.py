8dc5aca014244e9f8a6f4b7720ce9e01a5a8b7df
Sergio Forcen <sergiocalipso@gmail.com>
2022-01-03 15:22:55 +0100

    Create User model

--- cride/utils/models.py
+++ cride/utils/models.py
@@ -1,33 +1,35 @@
-# Django models utilities
+
+   
+"""Django models utilities."""
 
 # Django
 from django.db import models
 
+
 class CRideModel(models.Model):
     """Comparte Ride base model.
-
     CRideModel acts as an abstract base class from which every
     other model in the project will inherit. This class provides
     every table with the following attributes:
-        + created (DateTime): Store the datetime the objects was created
-        + modified (DateTime): Store the last datetime the objects was modified
+        + created (DateTime): Store the datetime the object was created.
+        + modified (DateTime): Store the last datetime the object was modified.
     """
 
     created = models.DateTimeField(
         'created at',
-        auto_now_add = True
-        help_text='Date time on which the object was ccreated.'
-        )
+        auto_now_add=True,
+        help_text='Date time on which the object was created.'
+    )
     modified = models.DateTimeField(
         'modified at',
-        auton_now = True
-        help_text='Date time on which the object was modified.'
-        )
+        auto_now=True,
+        help_text='Date time on which the object was last modified.'
+    )
 
     class Meta:
         """Meta option."""
 
-        abstract = True # Do not create a table in the DB for this class 
+        abstract = True
 
-        get_lasted_by = 'created'
-        orderinng = ['-created', '-modified']
+        get_latest_by = 'created'
+        ordering = ['-created', '-modified']
