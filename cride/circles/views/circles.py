2cedf33874fabc58bebb0b022684bb48ab4a8552
Sergio Forcen <sergiocalipso@gmail.com>
2022-01-09 12:50:21 +0100

    Added authorization and pagination in circles ViewSets

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -2,6 +2,7 @@
 
 # Django REST Framwork
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
@@ -12,3 +13,17 @@ from cride.circles.models import Circle
 class CircleViewSet(viewsets.ModelViewSet):
     queryset = Circle.objects.all()
     serializer_class = CircleModelSerializer
+    permission_classes =(IsAuthenticated,)
+
+    def get_queryset(self):
+        """Restrict list to public-only circles
+        only if a list is provided.
+        If only a single circle is provided 
+        do not apply filer.
+
+        """
+        queryset = Circle.objects.all()
+        if self.action == 'list':
+            return queryset.filter(is_public=True)
+        return queryset
+    
