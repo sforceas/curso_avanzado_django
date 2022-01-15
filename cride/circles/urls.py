407a15d7283cb4177b4450e3c095a44a3d5623cf
Sergio Forcen <sergiocalipso@gmail.com>
2022-01-15 11:54:44 +0100

    Add Memberships views

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -9,9 +9,13 @@ from rest_framework.routers import DefaultRouter
 
 # Views
 from cride.circles.views import circles as circles_views
+from cride.circles.views import memberships as memberships_views
+
 
 router = DefaultRouter()
 router.register(r'circles',circles_views.CircleViewSet,basename='circle')
+router.register(r'circles/(?P<slug_name>[a-zA-Z0-9_-]+)/members',memberships_views.MembershipViewSet,basename='membership')
+
 urlpatterns = [
     path('',include(router.urls))
 ]
